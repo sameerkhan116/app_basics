f3ac3d009af5fd1aaf2870a2926797c12ec093f6
Sameer Khan <sameer_khan@student.uml.edu>
2017-02-23 16:24:54 -0500

    Nearly there

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,11 +7,15 @@ package com.example.android.justjava;
  * package com.example.android.justjava;
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -27,31 +31,54 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    int quantity = 0;
+    int quantity = 2;
 
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, "You cannot order more than hundred cups of coffee.", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity += 1;
         display(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity == 1) {
+            Toast.makeText(this, "You need to order atleast one cup of coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity -= 1;
         display(quantity);
     }
 
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean addWhippedCream, boolean addChoch) {
+        int baseprice = 5;
+        if (addChoch) {
+            baseprice += 2;
+        }
+        if (addWhippedCream) {
+            baseprice += 1;
+        }
+        return quantity * baseprice;
     }
 
     public void submitOrder(View view) {
+        EditText getname = (EditText) findViewById(R.id.what_name);
+        String name = getname.getText().toString();
         CheckBox whippedCream = (CheckBox) findViewById(R.id.whipped_checkbox);
         boolean hasWhippedCream = whippedCream.isChecked();
         CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChoc = chocolate.isChecked();
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChoc);
-        displayMessage(priceMessage);
+        int price = calculatePrice(hasWhippedCream, hasChoc);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChoc, name);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null ) {
+            startActivity(intent);
+        }
 
     }
 
@@ -63,16 +90,8 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
-
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChoc) {
-        String priceMessage = "Name: Sameer Khan";
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChoc, String name) {
+        String priceMessage = "Name: " + name;
         priceMessage += "\nAdd whipped cream? " + addWhippedCream;
         priceMessage += "\nAdd chocolate? " + addChoc;
         priceMessage += "\nQuantity: " + quantity;
