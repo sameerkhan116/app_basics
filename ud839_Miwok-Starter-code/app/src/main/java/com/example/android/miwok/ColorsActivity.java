4866a01f89c9d5883640508781d24189862aa87d
Sameer Khan <sameer_khan@student.uml.edu>
2017-02-26 13:06:28 -0500

    All dictionaries added

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> colors = new ArrayList<Word>();
+        colors.add(new Word("weṭeṭṭi", "red"));
+        colors.add(new Word("chokokki", "green"));
+        colors.add(new Word("ṭakaakki", "brown"));
+        colors.add(new Word("ṭopoppi", "gray"));
+        colors.add(new Word("kululli", "black"));
+        colors.add(new Word("kelelli", "white"));
+        colors.add(new Word("ṭopiisә", "dusty yellow"));
+        colors.add(new Word("chiwiiṭә", "mustard yellow"));
+
+        WordAdapter adapter = new WordAdapter(this, colors);
+        ListView listView = (ListView) findViewById(R.id.colors);
+        listView.setAdapter(adapter);
     }
 }
--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        ArrayList<Word> family = new ArrayList<Word>();
+        family.add(new Word("әpә", "father"));
+        family.add(new Word("әṭa", "mother"));
+        family.add(new Word("angsi", "son"));
+        family.add(new Word("tune", "daughter"));
+        family.add(new Word("taachi", "older brother"));
+        family.add(new Word("chalitti", "younger brother"));
+        family.add(new Word("teṭe", "older sister"));
+        family.add(new Word("kolliti", "younger sister"));
+        family.add(new Word("ama", "grandmother"));
+        family.add(new Word("paapa", "grandfather"));
+
+        WordAdapter adapter = new WordAdapter(this, family);
+        ListView listView = (ListView) findViewById(R.id.family);
+        listView.setAdapter(adapter);
     }
 }
