ebf3640d7535c4d0cc0c34fc2828199a701e266c
Sameer Khan <sameer_khan@student.uml.edu>
2017-03-08 11:35:28 -0500

    Messaging SDK

---
+++ android-messaging-tutorial-skeleton-master/Messaging/src/main/java/com/sinch/messagingtutorialskeleton/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.sinch.messagingtutorialskeleton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.messagingtutorialskeleton.R;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
